afa41337513cf93d4c07d3173cbdf1aba7f24b6e
agusscript <agus.sanchez.240@gmail.com>
2025-05-03 23:43:16 -0300

    Add api service

---
+++ src/services/api.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { AuthService } from "./authService";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = AuthService.getToken();
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    const message =
+      error.response?.data?.message || "Ocurrió un error. Por favor intenta nuevamente.";
+
+    if (error.response?.status === 401) {
+      AuthService.clearToken();
+      window.location.href = "/login";
+    }
+
+    toast.error(message);
+    return Promise.reject(error);
+  },
+);
