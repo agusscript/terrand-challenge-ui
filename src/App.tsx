ef86b05be5582c171419c6e203982308779831d1
agusscript <agus.sanchez.240@gmail.com>
2025-05-07 14:03:46 -0300

    Add routes handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProtectedRoute from './components/ProtectedRoute';
+import Dashboard from './pages/Dashboard';
+import CreateRecipe from './pages/CreateRecipe';
+import EditRecipe from './pages/EditRecipe';
+import AllRecipes from './pages/AllRecipes';
+import ViewRecipe from './pages/ViewRecipe';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AuthProvider>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recipe/all" element={<AllRecipes />} />
+        <Route path="/recipe/:id" element={<ViewRecipe />} />
 
-export default App
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/recipe/create" element={<CreateRecipe />} />
+          <Route path="/recipe/edit/:id" element={<EditRecipe />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+}
